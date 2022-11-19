c3ec7e656c28e0696ef215c6ab55d30ba29f029e
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-20 00:19:24 +0600

    Update RMX_ENC.py

--- RMX_ENC.py
+++ RMX_ENC.py
@@ -1,11 +1,25 @@
-import os,platform
-os.system('clear')
+import os
+
+ 
+
+ 
 
 os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
 
-    os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import ENC
-else:
-    exit('\033[1;31m[×] Sorry Device Not Support')
+ 
+
+ 
+
+
+
+
+os.system('xdg-open https://facebook.com/groups/770617227293870/')
+ 
+
+
+ 
+
+import ENC
+
+
+ENC.MainMenu()
